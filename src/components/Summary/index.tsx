28366d35d07228f29474034125810fbf9fe34fac
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2021-10-02 16:38:58 -0300

    Componente TransactionsTable

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,21 +11,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>R$12.000,00</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>R$1.000,00</strong>
       </div>
       <div>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>R$11.000,00</strong>
       </div>
     </Container>
   );
