3fa2e82e8d2560c8bfdfde32e55390908759030c
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2021-10-02 18:06:58 -0300

    Configurando modal de criaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const[isNewTransactionsModal, setIsNewTransactionsModal] = useState(false)
+
+  function handlesOpenNewTransactionsModal() {
+    setIsNewTransactionsModal(true);
+  }
+
+  function handlesCloseNewTransactionsModal() {
+    setIsNewTransactionsModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handlesOpenNewTransactionsModal  }/>
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionsModal}
+        onRequestClose={handlesCloseNewTransactionsModal}
+      >
+        <h2>Cadastrar informação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
