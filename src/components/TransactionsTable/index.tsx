a214fc4fbf5f7c1f31ce0b3974eed2d525a641d4
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2021-10-09 14:00:39 -0300

    Adicionando sinal de retirada e adiçao

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -39,12 +39,21 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>
-                {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL'
-                }).format(transaction.amount)}
-              </td>
+              {transaction.type === 'withdraw' ?
+                <td className={transaction.type}>
+                  -{new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+                </td>
+                :
+                <td className={transaction.type}>
+                  +{new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+                </td>
+              }
               <td>{transaction.category}</td>
               <td>
                 {new Intl.DateTimeFormat('pt-BR').format(
